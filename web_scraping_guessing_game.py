cee4a91cf3e36dfe390f3d414895fe7eb7c5cc7f
Joel Bebo <joelbebo@gmail.com>
2019-06-04 11:08:02 -0400

    initial commit

---
+++ web_scraping_guessing_game.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+all_quotes = []
+res = requests.get(f"http://quotes.toscrape.com")
+soup = BeautifulSoup(res.text, "html.parser")
+quotes = soup.find_all(call_="quote")
+for quote in quotes:
+    all_quotes.append({
+        "text": quote.find(class_="text").get_text(),
+        "auther": quote.find(class_="auther").get_text()
+        })
+
+print(all_quotes)
