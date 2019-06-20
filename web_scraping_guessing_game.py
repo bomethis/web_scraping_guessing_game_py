2ba9c20004a2c07ff3904bd70d6a97a5906d5913
Joel Bebo <joelbebo@gmail.com>
2019-06-20 16:51:11 -0400

    anchor tag bio link

--- web_scraping_guessing_game.py
+++ web_scraping_guessing_game.py
@@ -8,7 +8,8 @@ quotes = soup.find_all(call_="quote")
 for quote in quotes:
     all_quotes.append({
         "text": quote.find(class_="text").get_text(),
-        "auther": quote.find(class_="auther").get_text()
+        "auther": quote.find(class_="auther").get_text(),
+        "bio-link": quote.find(class_="a")["href"]
         })
 
 print(all_quotes)
