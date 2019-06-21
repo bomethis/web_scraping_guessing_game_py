1e4817b4e0793db660093fc8d1171c0d0f36b4fc
Joel Bebo <joelbebo@gmail.com>
2019-06-20 21:17:02 -0400

    provide hints to the player

--- web_scraping_guessing_game.py
+++ web_scraping_guessing_game.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from time import sleep
+from random import choice
 import requests
 
 all_quotes = []
@@ -9,7 +10,7 @@ url = "/page/1"
 while url:
 
     res = requests.get(f"{base_url}{url}")
-    print(f"Now Scraping {base_url}{url}...")
+    # print(f"Now Scraping {base_url}{url}...")
     soup = BeautifulSoup(res.text, "html.parser")
     quotes = soup.find_all(class_="quote")
 
@@ -22,8 +23,28 @@ while url:
 
     next_btn = soup.find(class_="next")
     url = next_btn.find("a")["href"] if next_btn else None
-    sleep(2)
-print(all_quotes)
-# print(next_btn.find("a")["href"])
+    # sleep(1)
 
-# print(all_quotes)
+quote = choice(all_quotes)
+remaining_guesses = 4
+print("Here's a quote: ")
+print(quote["text"])
+
+guess =''
+while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+    guess = input(f"Who said this quote? Guesses remaining: {remaining_guesses}  ")
+    remaining_guesses -= 1
+    if remaining_guesses == 3:
+        res = requests.get(f"{base_url}{quote['bio-link']}")
+        soup = BeautifulSoup(res.text, "html.parser")
+        birth_date = soup.find(class_="author-born-date").get_text()
+        birth_place = soup.find(class_="author-born-location").get_text()
+        print(f"Here's a hint: The author was born on {birth_date} {birth_place}")
+    elif remaining_guesses == 2:
+        print(f"Here's a hint: The author's first name starts with: {quote['author'][0]}")
+    elif remaining_guesses == 1:
+        last_initial = quote['author'].split(" ")[1][0]
+        print(f"Here's a hint: The author's last name starts with: {last_initial}")
+    else:
+        print(f"Sorry you guesses remaining. The answer was {quote['author']}")
+print("AFTER WHILE LOOP")
